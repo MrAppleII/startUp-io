457d82e08fc02fc4a1e1a316597399b8fdcf48b2
Jacob Killelea <jkillelea@protonmail.ch>
2018-08-12 12:27:42 -0700

    formatting

--- js/generator.js
+++ js/generator.js
@@ -1,17 +1,14 @@
-var markets = ["retail","marketing","Dating","video","coffee","travel"];
-var technologies = ["AI","Blockchain","Machine Learning","Mobile"];
-var olderStartups =["Uber","Airbnb","Netflix"];
+var markets       = ["retail", "marketing", "Dating", "video", "coffee", "travel"];
+var technologies  = ["AI", "Blockchain", "Machine Learning", "Mobile"];
+var olderStartups = ["Uber", "Airbnb", "Netflix"];
 
-var numberForCase = Math.floor(Math.random() * 2);      // returns a random integer from 0 to 2
-
-var marketGenNumber = Math.floor(Math.random() * (markets.length()-1)); // returns a random integer from 0 to length of array 
-var marketToPlug = markets[marketGenNumber]; //set the string to the one from the array using the random number 
-
-var techGenNumber = Math.floor(Math.random() * (technologies.length()-1)); // returns a random integer from 0 to length of array 
-var techToPlug = technologies[techGenNumber]; //set the string to the one from the array using the random number 
-
+var numberForCase            = Math.floor(Math.random() * 2);                          // returns a random integer from 0 to 2
+var marketGenNumber          = Math.floor(Math.random() * (markets.length()-1));       // returns a random integer from 0 to length of array 
+var marketToPlug             = markets[marketGenNumber];                               //set the string to the one from the array using the random number 
+var techGenNumber            = Math.floor(Math.random() * (technologies.length()-1));  // returns a random integer from 0 to length of array 
+var techToPlug               = technologies[techGenNumber];                            //set the string to the one from the array using the random number 
 var existingStartupGenNumber = Math.floor(Math.random() * (olderStartups.length()-1)); // returns a random integer from 0 to length of array 
-var existingStartupToPlug = technologies[existingStartupGenNumber]; //set the string to the one from the array using the random number 
+var existingStartupToPlug    = technologies[existingStartupGenNumber];                 //set the string to the one from the array using the random number 
 
 var startupIdea = "";
 
