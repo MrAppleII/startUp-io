c35cbebcb42f8bf8cbcf7da7a2a8a15fe49c7e25
Jacob Killelea <jkillelea@protonmail.ch>
2018-08-12 12:57:09 -0700

    formatting

--- js/generator.js
+++ js/generator.js
@@ -1,34 +1,32 @@
 function generateName(){
+    var markets       = ["retail","marketing","Dating","video","coffee","travel"];
+    var technologies  = ["AI","Blockchain","Machine Learning","Mobile"];
+    var olderStartups = ["Uber","Airbnb","Netflix"];
 
+    var number = Math.floor(Math.random() * 2); // returns a random integer from 0 to 2
 
-var markets       = ["retail","marketing","Dating","video","coffee","travel"];
-var technologies  = ["AI","Blockchain","Machine Learning","Mobile"];
-var olderStartups = ["Uber","Airbnb","Netflix"];
+    var marketGenNumber = Math.floor(Math.random() * (markets.length-1)); // returns a random integer from 0 to length of array
+    var marketToPlug    = markets[marketGenNumber];                         //set the string to the one from the array using the random number
 
-var number = Math.floor(Math.random() * 2); // returns a random integer from 0 to 2
+    var techGenNumber = Math.floor(Math.random() * (technologies.length-1)); // returns a random integer from 0 to length of array
+    var techToPlug    = technologies[techGenNumber];                           //set the string to the one from the array using the random number
 
-var marketGenNumber = Math.floor(Math.random() * (markets.length-1)); // returns a random integer from 0 to length of array
-var marketToPlug    = markets[marketGenNumber];                         //set the string to the one from the array using the random number
+    var existingStartupGenNumber = Math.floor(Math.random() * (olderStartups.length-1)); // returns a random integer from 0 to length of array
+    var existingStartupToPlug    = technologies[existingStartupGenNumber];                 //set the string to the one from the array using the random number
 
-var techGenNumber = Math.floor(Math.random() * (technologies.length-1)); // returns a random integer from 0 to length of array
-var techToPlug    = technologies[techGenNumber];                           //set the string to the one from the array using the random number
+    var startupIdea = "";
 
-var existingStartupGenNumber = Math.floor(Math.random() * (olderStartups.length-1)); // returns a random integer from 0 to length of array
-var existingStartupToPlug    = technologies[existingStartupGenNumber];                 //set the string to the one from the array using the random number
+    if(number == 0){
+        startupIdea = "It is " + marketToPlug + " with the power of " + techToPlug + ".";
+    }
 
-var startupIdea = "";
+    if(number == 1){
+        startupIdea = "Its " + techToPlug + " for the " + marketToPlug + ".";
+    }
 
-if(number == 0){
-    startupIdea = "It is " + marketToPlug + " with the power of " + techToPlug + ".";
-}
-
-if(number == 1){
-    startupIdea = "Its " + techToPlug + " for the " + marketToPlug + ".";
-}
-
-if(number == 2){
-    startupIdea  = "The " + existingStartupToPlug + " for " + marketToPlug + ".";
-}
+    if(number == 2){
+        startupIdea  = "The " + existingStartupToPlug + " for " + marketToPlug + ".";
+    }
 
 return startupIdea;
 }
